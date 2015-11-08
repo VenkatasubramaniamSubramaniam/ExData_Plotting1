bcde7515132410d091f2b97831d8683f7602873a
Subbu <VenkatasubramaniamSubramaniam@users.noreply.github.com>
2015-11-08 17:35:54 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,1 +1,15 @@
+#plot3.R - Creates a plot on screen and saves it as png file
 
+## Getting full dataset
+readingCompleteFile <- read.csv(".\\R_WorkingDirectory\\data\\household_power_consumption.txt"
+                                , header=T, sep=';', na.strings="?", check.names=F, stringsAsFactors=F
+                                , comment.char="", quote='\"')
+readingCompleteFile$Date <- as.Date(readingCompleteFile$Date, format="%d/%m/%Y")
+
+## Subsetting the data
+data <- subset(readingCompleteFile, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(readingCompleteFile)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
